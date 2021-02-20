c6b2dd4e4df695b18737015fe3e32a585ad98f55
Kala Foods <guhan.delta@gmail.com>
2021-02-20 09:22:15 -0500

    Simplified paths to imported files by providing absolute paths using jsconfig.json => compilerOptions

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import '../styles/index.scss'
+import 'styles/index.scss'
 
 export default ({ Component, pageProps }) => <Component {...pageProps} />
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Nav, Media, Image, Card } from 'react-bootstrap';
-import { Navbar } from './components';
+import { Navbar } from 'components';
 
 export default function Home(){
   return (
