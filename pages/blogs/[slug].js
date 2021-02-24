c9a985f00590ff236d2f1992ddfd878b5674fd2e
Kala Foods <guhan.delta@gmail.com>
2021-02-24 09:07:49 -0500

    Generated static paths for all teh static generated pages

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -10,12 +10,35 @@ const BlogDetail = ({ blog }) => {
     )
 }
 
-export async function getServerSideProps({ params }){
+export async function getStaticProps({ params }){
     const blog = await getBlogBySlug(params.slug);
     return{
         props: { 
             blog
-         }
+        }
+    }
+}
+
+// If a page has dynamic routes (documentation) and uses getStaticProps it needs to define a list of paths that have to be rendered to HTML at build time.
+// If you export an async function called getStaticPaths from a page that uses dynamic routes, Next.js will statically pre-render all the paths specified by getStaticPaths.
+// getStaticPaths() will be called for/exe on each page
+export function getStaticPaths(){
+    console.log('====================================');
+    console.log('Getting paths for every page');
+    console.log('====================================');
+    return{
+        paths:[
+            {params: {
+                slug: 'my-first-blog'
+            }},
+            {params: {
+                slug: 'my-second-blog'
+            }},
+            {params: {
+                slug: 'my-third-blog'
+            }},
+        ],
+        fallback: false // returning fallback is mandatory, false => land on the default 404 page if page not found
     }
 }
 
