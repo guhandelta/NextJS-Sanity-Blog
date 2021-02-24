d36e35bc5d74ea634845ec79f99de652705636a5
Kala Foods <guhan.delta@gmail.com>
2021-02-24 07:43:02 -0500

    Added functionality to fetch blogs by slug

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,14 +1,22 @@
-import { useRouter } from 'next/router';
 import { Layout } from '../../components';
+import { getBlogBySlug } from '../../lib/api';
 
-const BlogDetail = () => {
-    const { query } = useRouter();
+const BlogDetail = ({ blog }) => {
     debugger;
     return (
         <Layout>
-            <h1>Detail page for {query?.slug}</h1>
+            <h1>Detail page for {blog?.slug}</h1>
         </Layout>
     )
+}
+
+export async function getServerSideProps({ params }){
+    const blog = await getBlogBySlug(params.slug);
+    return{
+        props: { 
+            blog
+         }
+    }
 }
 
 export default BlogDetail;
