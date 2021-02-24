4dfbb3f04d8433bd8128eacb8a4a7caca97a9538
Kala Foods <guhan.delta@gmail.com>
2021-02-24 09:23:29 -0500

    Dynamically retrieved the slugs from the posts in Sanity, to create StaticPaths

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,5 +1,5 @@
 import { Layout } from '../../components';
-import { getBlogBySlug } from '../../lib/api';
+import { getBlogBySlug, getAllBlogs } from '../../lib/api';
 
 const BlogDetail = ({ blog }) => {
     debugger;
@@ -22,22 +22,11 @@ export async function getStaticProps({ params }){
 // If a page has dynamic routes (documentation) and uses getStaticProps it needs to define a list of paths that have to be rendered to HTML at build time.
 // If you export an async function called getStaticPaths from a page that uses dynamic routes, Next.js will statically pre-render all the paths specified by getStaticPaths.
 // getStaticPaths() will be called for/exe on each page
-export function getStaticPaths(){
-    console.log('====================================');
-    console.log('Getting paths for every page');
-    console.log('====================================');
-    return{
-        paths:[
-            {params: {
-                slug: 'my-first-blog'
-            }},
-            {params: {
-                slug: 'my-second-blog'
-            }},
-            {params: {
-                slug: 'my-third-blog'
-            }},
-        ],
+export async function getStaticPaths(){
+    const blogs = await getAllBlogs();
+    // const paths =  blogs?.map(blog=>({params: {slug: blog.slug}}));
+    return {
+        paths: blogs?.map(blog=>({params: {slug: blog.slug}})),
         fallback: false // returning fallback is mandatory, false => land on the default 404 page if page not found
     }
 }
