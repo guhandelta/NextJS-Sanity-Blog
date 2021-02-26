2a7263c3d525965fa93a0fcde1f92b32becf9dc8
Kala Foods <guhan.delta@gmail.com>
2021-02-26 18:30:08 -0500

    Added FontAwesome Icons for view change button

--- components/FilteringMenu.js
+++ components/FilteringMenu.js
@@ -1,11 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// +!filter.view.list =      0        |  1
+const LIST_VIEW_ICONS = ['list', 'border-all' ]
 
 const FilteringMenu = ({ onChange, filter }) => {
     return (
         <div className="filtering-menu mb-2">
-            <div onClick={() => onChange('view', { list: +!filter.view.list })}> {/* + => numeric val || +! => numeric val of inverted */}
+            <FontAwesomeIcon 
+                className="clickable hoverable"
+                size="lg"
+                icon={LIST_VIEW_ICONS[filter.view.list]}
+                onClick={() => {
+                    onChange('view', { list: +!filter.view.list })}}
+            > 
                 Change view
-            </div>
+            </FontAwesomeIcon>
         </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'highlight.js/styles/darcula.css'
-import 'styles/index.scss'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { faBorderAll, faList } from '@fortawesome/free-solid-svg-icons'
+
+config.autoAddCss = false; //To inform FOntAwesome to skip adding the css file, as it is added manually, below, in #7
+library.add(faBorderAll, faList);
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'highlight.js/styles/darcula.css';
+import 'styles/index.scss';
 
 export default ({ Component, pageProps }) => <Component {...pageProps} />
 
