d099705d2def47418bd19e2074e916ad50f7cb16
Kala Foods <guhan.delta@gmail.com>
2021-02-26 22:07:21 -0500

    Used SWR - Stale-While-Revalidate to prevent CORS error, by fetching data, by using a function, that will be exe on the server

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import { Row, Col } from 'react-bootstrap';
 import { AuthorIntro, CardItem, CardListItem, Layout, FilteringMenu } from 'components';
+// SWR - Stale-While-Revalidate => Provide data from cache, while the req sent in parallel will fetch the updated data adn update the UI with-
+//- with latest data, once res arrives
+import useSWR from 'swr';
 
 import { getAllBlogs } from '../lib/api';
 import { useState } from 'react';
+
+// Making a req to fetch more blogs througha useEffect(()=>{},[]) will throw a CORS error
+// This fn() will will make a call ti lib/api, which will be executed in the server => preventing CORS error
+const fetcher = url => fetch(url).then(res => res.json());
 
 export default function Home({blogs}){
   // debugger;
   const [ filter, setFilter ] = useState({
     view: { list: 0  }
   });
+
+  const { data, error } = useSWR('/api/hello', fetcher);
+  debugger;
+
   return (
     <Layout>
         <AuthorIntro />
@@ -65,7 +76,7 @@ export default function Home({blogs}){
 
 // This function is called during Build Time, on the server and never called on the client 
 // Provides props to the page and builds a static page
-// During each Recompile, the getStaticProps() creates a new index.HTML file for index.js
+// During each Recompile, the getStaticProps() creates a new index.HTML file for index.js || Request is made from the server
 export async function getStaticProps(){
   const blogs = await getAllBlogs();
   return{
